b9409521dc17c3d9e15d56dca35dbec183dda094
Andre Davcev <>
2021-03-12 00:35:25 -0500

    Add basic Nebular layout

--- apps/torcoin/src/app/app.module.ts
+++ apps/torcoin/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbSidebarModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, NbThemeModule.forRoot({ name: 'corporate' }), NbLayoutModule, NbEvaIconsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'corporate' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbSidebarModule.forRoot(),
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
