781ef64672f48c540b75e8f43a1d11a973269a86
Andre Davcev <>
2021-03-13 16:46:07 -0500

    Create torcoin app library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/torcoin', '<rootDir>/libs/core'],
+  projects: [
+    '<rootDir>/apps/torcoin',
+    '<rootDir>/libs/core',
+    '<rootDir>/libs/app/torcoin',
+  ],
 };
